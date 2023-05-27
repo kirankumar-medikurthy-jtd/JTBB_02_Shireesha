b6ca9434ad6ac83bd2ed51b48591d8e88375669c
shireesha9347 <shireeshareddy.b2002@gmail.com>
2023-05-28 00:33:43 +0530

    evaluation done

---
+++ week_06/day_1/evaluation/index.js
@@ -0,0 +1,12 @@
+
+function DelayTime(){
+    let button=document.getElementById("button");
+    let fname=document.querySelector(".fname").value;
+    let lname=document.querySelector(".lname").value;
+    async function DelayTime (){
+        let search = document.querySelector("#button").value;
+        button.innerHTML="loading...";
+    
+    }
+    
+}
